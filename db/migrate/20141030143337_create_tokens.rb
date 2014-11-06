4483791121f45285e9203bdb6d39473800078625
llleelay <llleelay@gmail.com>
2014-11-06 22:24:31 +0800

    remove INDEX for token

--- db/migrate/20141030143337_create_tokens.rb
+++ db/migrate/20141030143337_create_tokens.rb
@@ -2,14 +2,13 @@ class CreateTokens < ActiveRecord::Migration
   def change
     create_table :tokens do |t|
       t.belongs_to :user
-      t.string     :token
+      t.text       :token
       t.datetime   :expire
 
       t.timestamps
     end
 
     add_index(:tokens, :user_id, unique: true)
-    add_index(:tokens, :token,   unique: true)
 
   end
 end
